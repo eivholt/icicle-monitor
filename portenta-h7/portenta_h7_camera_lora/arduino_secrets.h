236ddd3f974517e4034ae64354992f3d43423f5a
Eivind Holt <eivholt@gmail.com>
2024-04-14 08:40:50 +0200

    corrections

--- portenta-h7/portenta_h7_camera_lora/arduino_secrets.h
+++ portenta-h7/portenta_h7_camera_lora/arduino_secrets.h
@@ -1,3 +1,3 @@
 // Replace with keys obtained from TheThingsNetwork console
-#define SECRET_APP_EUI "0000000000000000"
-#define SECRET_APP_KEY "ED35BF08F706CA5C7B9981A0C8787AA6"
+#define SECRET_APP_EUI "xxxxxxxxxxxxx"
+#define SECRET_APP_KEY "yyyyyyyyyyyyyyyyyyyyyyy"
